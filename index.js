5068f2c736c7c9b01943f784b9c77f67c8e94604
Jay Proulx <jproulx@tmg.io>
2019-09-27 15:13:38 -0400

    Fix submodule paths

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-    AppExports: require('lib/AppExports'),
-    ApplicationParameters: require('lib/ApplicationParameters'),
-    CloudFormationHelper: require('lib/CloudFormationHelper'),
-    SSMParameter: require('lib/SSMParameter')
+    AppExports: require('./lib/AppExports'),
+    ApplicationParameters: require('./lib/ApplicationParameters'),
+    CloudFormationHelper: require('./lib/CloudFormationHelper'),
+    SSMParameter: require('./lib/SSMParameter')
 };
